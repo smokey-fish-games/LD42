c4fa6a70662d8a398c863ff6b858208e9e8fb770
Rob Parker <jade_monkey_7@hotmail.com>
2018-08-11 22:38:02 +0100

    more shit

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("I AM ALIVE)");
+		Animator a = this.GetComponent<Animator>();
+		a.Play("SwipeAttack");
+		Destroy(gameObject, a.GetCurrentAnimatorStateInfo(0).length);
+	}
+}
