ad3ee66d85b79aa68456308262379fca7ff60215
Rob Parker <jade_monkey_7@hotmail.com>
2018-08-11 08:27:30 +0100

    Basic movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,13 +4,76 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public enum DIRECTION { UP, DOWN, LEFT, RIGHT,UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT };
+	public bool MOVEUP = false;
+	public bool MOVEDOWN = false;
+	public bool MOVELEFT = false;
+	public bool MOVERIGHT = false;
+	public float speed = 0.5f;
+
+	public void Move(DIRECTION direction) {
+		Vector2 currentPos = transform.position;
+		Vector2 newpos = new Vector2();
+		switch (direction) {
+			case DIRECTION.UP:
+				Debug.Log("Moving up!");
+				newpos = currentPos + new Vector2(0, speed * Time.deltaTime);
+				break;
+			case DIRECTION.DOWN:
+				Debug.Log("Moving Down!");
+				newpos = currentPos + new Vector2(0, -speed * Time.deltaTime);
+				break;
+			case DIRECTION.LEFT:
+				Debug.Log("Moving Left!");
+				newpos = currentPos + new Vector2(-speed * Time.deltaTime, 0);
+				break;
+			case DIRECTION.RIGHT:
+				Debug.Log("Moving Right!");
+				newpos = currentPos + new Vector2(speed * Time.deltaTime, 0);
+				break;
+
+			case DIRECTION.UPLEFT:
+				Debug.Log("Moving up Left!");
+				newpos = currentPos + new Vector2(0, 0);
+				break;
+			case DIRECTION.UPRIGHT:
+				Debug.Log("Moving Up Right!");
+				newpos = currentPos + new Vector2(0, 0);
+				break;
+			case DIRECTION.DOWNLEFT:
+				Debug.Log("Moving Down Left!");
+				newpos = currentPos + new Vector2(0, 0);
+				break;
+			case DIRECTION.DOWNRIGHT:
+				Debug.Log("Moving Down Right!");
+				newpos = currentPos + new Vector2(0, 0);
+				break;
+
+			default:
+				Debug.Log("TODO " + direction);
+				break;
+		}
+		transform.position = newpos;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		if( MOVEUP ){
+			Move(DIRECTION.UP);
+		}
+		if( MOVEDOWN ){
+			Move(DIRECTION.DOWN);
+		}
+		if( MOVELEFT ){
+			Move(DIRECTION.LEFT);
+		}
+		if( MOVERIGHT ){
+			Move(DIRECTION.RIGHT);
+		}
 		
 	}
 }
