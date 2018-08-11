86091a7dc611c5d3115e287e3ebe38c26d37b09c
Josie Messa <josie.messa@multiplay.co.uk>
2018-08-11 08:49:42 +0100

    Fix camera viewing blob bug and line endings

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -2,85 +2,105 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
 
-	public enum DIRECTION { UP, DOWN, LEFT, RIGHT,UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT };
-	public bool MOVEUP = false;
-	public bool MOVEDOWN = false;
-	public bool MOVELEFT = false;
-	public bool MOVERIGHT = false;
-	public float speed = 0.5f;
+    public enum DIRECTION { UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT };
+    public bool MOVEUP = false;
+    public bool MOVEDOWN = false;
+    public bool MOVELEFT = false;
+    public bool MOVERIGHT = false;
+    public float speed = 0.5f;
 
-	public void Move(DIRECTION direction) {
-		Vector2 currentPos = transform.position;
-		Vector2 newpos = new Vector2();
-		float dirMove = (speed * Time.deltaTime) / 2;
-		switch (direction) {
-			case DIRECTION.UP:
-				Debug.Log("Moving up!");
-				newpos = currentPos + new Vector2(0, speed * Time.deltaTime);
-				break;
-			case DIRECTION.DOWN:
-				Debug.Log("Moving Down!");
-				newpos = currentPos + new Vector2(0, -speed * Time.deltaTime);
-				break;
-			case DIRECTION.LEFT:
-				Debug.Log("Moving Left!");
-				newpos = currentPos + new Vector2(-speed * Time.deltaTime, 0);
-				break;
-			case DIRECTION.RIGHT:
-				Debug.Log("Moving Right!");
-				newpos = currentPos + new Vector2(speed * Time.deltaTime, 0);
-				break;
+    public void Move(DIRECTION direction)
+    {
+        Vector2 currentPos = transform.position;
+        Vector2 newpos = new Vector2();
+        float dirMove = (speed * Time.deltaTime) / 2;
+        switch (direction)
+        {
+            case DIRECTION.UP:
+                Debug.Log("Moving up!");
+                newpos = currentPos + new Vector2(0, speed * Time.deltaTime);
+                break;
+            case DIRECTION.DOWN:
+                Debug.Log("Moving Down!");
+                newpos = currentPos + new Vector2(0, -speed * Time.deltaTime);
+                break;
+            case DIRECTION.LEFT:
+                Debug.Log("Moving Left!");
+                newpos = currentPos + new Vector2(-speed * Time.deltaTime, 0);
+                break;
+            case DIRECTION.RIGHT:
+                Debug.Log("Moving Right!");
+                newpos = currentPos + new Vector2(speed * Time.deltaTime, 0);
+                break;
 
-			case DIRECTION.UPLEFT:
-				Debug.Log("Moving up Left!");
-				newpos = currentPos + new Vector2(-dirMove, dirMove);
-				break;
-			case DIRECTION.UPRIGHT:
-				Debug.Log("Moving Up Right!");
-				newpos = currentPos + new Vector2(dirMove, dirMove);
-				break;
-			case DIRECTION.DOWNLEFT:
-				Debug.Log("Moving Down Left!");
-				newpos = currentPos + new Vector2(-dirMove, -dirMove);
-				break;
-			case DIRECTION.DOWNRIGHT:
-				Debug.Log("Moving Down Right!");
-				newpos = currentPos + new Vector2(dirMove, -dirMove);
-				break;
+            case DIRECTION.UPLEFT:
+                Debug.Log("Moving up Left!");
+                newpos = currentPos + new Vector2(-dirMove, dirMove);
+                break;
+            case DIRECTION.UPRIGHT:
+                Debug.Log("Moving Up Right!");
+                newpos = currentPos + new Vector2(dirMove, dirMove);
+                break;
+            case DIRECTION.DOWNLEFT:
+                Debug.Log("Moving Down Left!");
+                newpos = currentPos + new Vector2(-dirMove, -dirMove);
+                break;
+            case DIRECTION.DOWNRIGHT:
+                Debug.Log("Moving Down Right!");
+                newpos = currentPos + new Vector2(dirMove, -dirMove);
+                break;
 
-			default:
-				Debug.Log("TODO " + direction);
-				break;
-		}
-		transform.position = newpos;
-	}
+            default:
+                Debug.Log("TODO " + direction);
+                break;
+        }
+        transform.position = newpos;
+    }
 
 
-	/// <summary>
-	/// Update is called every frame, if the MonoBehaviour is enabled.
-	/// </summary>
-	void Update()
-	{
-		if( MOVEUP && MOVELEFT ){
-			Move(DIRECTION.UPLEFT);
-		} else if (MOVEUP && MOVERIGHT) {
-			Move(DIRECTION.UPRIGHT);
-		} else if( MOVEDOWN && MOVELEFT ){
-			Move(DIRECTION.DOWNLEFT);
-		} else if( MOVEDOWN && MOVERIGHT){
-			Move(DIRECTION.DOWNRIGHT);
-		} else if (MOVEDOWN ){
-			Move(DIRECTION.DOWN);
-		} else if (MOVEUP) {
-			Move(DIRECTION.UP);
-		} else if( MOVELEFT ){
-			Move(DIRECTION.LEFT);
-		} else if( MOVERIGHT ){
-			Move(DIRECTION.RIGHT);
-		} else {
-			Debug.Log("What?");
-		}
-	}
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (MOVEUP && MOVELEFT)
+        {
+            Move(DIRECTION.UPLEFT);
+        }
+        else if (MOVEUP && MOVERIGHT)
+        {
+            Move(DIRECTION.UPRIGHT);
+        }
+        else if (MOVEDOWN && MOVELEFT)
+        {
+            Move(DIRECTION.DOWNLEFT);
+        }
+        else if (MOVEDOWN && MOVERIGHT)
+        {
+            Move(DIRECTION.DOWNRIGHT);
+        }
+        else if (MOVEDOWN)
+        {
+            Move(DIRECTION.DOWN);
+        }
+        else if (MOVEUP)
+        {
+            Move(DIRECTION.UP);
+        }
+        else if (MOVELEFT)
+        {
+            Move(DIRECTION.LEFT);
+        }
+        else if (MOVERIGHT)
+        {
+            Move(DIRECTION.RIGHT);
+        }
+        else
+        {
+            Debug.Log("What?");
+        }
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
