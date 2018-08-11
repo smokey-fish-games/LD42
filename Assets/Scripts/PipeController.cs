1f083a8ada29089d40ee3e98bba239ec4e76129f
Rob Parker <jade_monkey_7@hotmail.com>
2018-08-11 10:09:44 +0100

    Rob Rebase

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour {
+
+	public Transform destination;
+
+	void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("PIPE!");
+    }
+}
