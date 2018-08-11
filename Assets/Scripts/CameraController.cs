77ef1691fffc0fb90522fe3e52ed952c44afdb7e
Rob Parker <jade_monkey_7@hotmail.com>
2018-08-11 13:54:54 +0100

    Update project settings and player model

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Set the resolution to what we want...
+		Screen.SetResolution(1024,768,false);
+	}
+}
