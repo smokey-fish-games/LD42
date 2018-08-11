a6368b574306dde89df3730870f0f03ea1ce1e6a
Chris <chris.bean91@gmail.com>
2018-08-11 15:21:57 +0100

    few modifcations to script

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -21,28 +21,41 @@ public class BoardManager : MonoBehaviour {
     public IntVector2 level_size = new IntVector2(8, 8);
 
     public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
 
     private Transform boardHolder;
 
-    private List <Vector3> gridPositions = new List <Vector3> ();
+    // private List <Vector3> gridPositions = new List <Vector3> ();
 
-    void initList() {
-        gridPositions.Clear();
+    // void initList() {
+    //     gridPositions.Clear();
 
-        for (int x = 0; x < level_size.X; x++) {
-            for (int y = 0; y < level_size.Y; y++) {
-                gridPositions.Add(new Vector3(x, y, 0f));
-            }
-        }
-    }
+    //     for (int x = 0; x < level_size.X; x++) {
+    //         for (int y = 0; y < level_size.Y; y++) {
+    //             gridPositions.Add(new Vector3(x, y, 0f));
+    //         }
+    //     }
+    // }
 
     void initBoard() 
     {
         boardHolder = new GameObject("Board").transform;
 
-        for (int x = 0; x < level_size.X; x++) {
-            for (int y = 0; y < level_size.Y; y++) {
-                GameObject toInit = floorTiles[Random.Range(0, floorTiles.Length)];
+        for (int x = -1; x < level_size.X +1; x++) {
+            for (int y = -1; y < level_size.Y +1; y++) {
+                
+                GameObject toInit;
+                
+                if (x == -1 || x == level_size.X || y == -1 || y == level_size.Y) 
+                {
+                    Debug.Log("Select wal");
+                    toInit = wallTiles[Random.Range(0, wallTiles.Length)];
+                }
+                else {
+                    Debug.Log("Select floor");
+                    toInit = floorTiles[Random.Range(0, floorTiles.Length)];
+                }
+
 
                 Bounds bounds = toInit.GetComponent<Renderer>().bounds;
 
@@ -58,6 +71,6 @@ public class BoardManager : MonoBehaviour {
 
     public void SetupScene (int level) {
         initBoard();
-        initList();
+        //initList();
     }
 }
