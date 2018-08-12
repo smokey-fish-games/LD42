72f77195ec933ff6000ab3edd5d031157ed76be1
Josie Messa <josie.messa@multiplay.co.uk>
2018-08-12 21:33:02 +0100

    sort out enemy movement and attacks

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Enemy : MovingObject
+public class Enemy : MovingObject
 {
     private Transform target;
-    abstract public float cooldown { get; set; }
-    public int health = 3;
+    public int health = 1;
+    public int cooldown = 2;
     private float attackCountdown;
-
-    // Use this for initialization
     protected override void Start()
     {
-        // this may blow up?
-        // animator = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
         if (target == null)
             Debug.Log("player not found");
-
         base.Start();
+    }
+
+
+    void Update()
+    {
+        MoveEnemy();
+        attackCountdown -= Time.deltaTime;
     }
 
     // MoveEnemy tries to move the current enemy sprite towards
@@ -29,7 +31,7 @@ public abstract class Enemy : MovingObject
         int yDir = 0;
 
         // are we in the same column as our player?
-        if (Mathf.Abs(target.position.x - transform.position.x) - detectionRadius < float.Epsilon)
+        if (Mathf.Abs(target.position.x - transform.position.x) - base.detectionRadius < float.Epsilon)
         {
             yDir = target.position.y > transform.position.y ? 1 : -1;
         }
@@ -37,6 +39,7 @@ public abstract class Enemy : MovingObject
         {
             xDir = target.position.x > transform.position.x ? 1 : -1;
         }
+
         AttemptMove<PlayerController>(xDir, yDir);
     }
 
@@ -44,9 +47,9 @@ public abstract class Enemy : MovingObject
     // does not move but attacks instead
     protected override void AttemptMove<T>(int xDir, int yDir)
     {
+
         base.AttemptMove<T>(xDir, yDir);
     }
-
 
     // OnCantMove is called from AttemptMove and attacks the player if it
     // is encountered
@@ -55,7 +58,7 @@ public abstract class Enemy : MovingObject
         if (attackCountdown > 0)
             return;
         PlayerController hitPlayer = component as PlayerController;
-        Debug.Log("hit player");
+        Debug.Log("attack player");
         hitPlayer.attackMe(1);
         // TODO: attack animation
         attackCountdown = cooldown;
@@ -67,6 +70,12 @@ public abstract class Enemy : MovingObject
         if (health == 0)
         {
             Debug.Log("enemy destroyed");
+            Destroy(gameObject);
         }
+    }
+
+    public void OnDeath()
+    {
+        Debug.Log("I'm dyyiinngg");
     }
 }
